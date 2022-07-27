a7640023b9fe4e43c49ce95422fa05968699fe3b
Julianne Knott <julesk1998@gmail.com>
2022-07-27 18:14:35 +0000

    fix rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,6 @@
-cd /datadrive/jknott/FasterTransformer/build
+cd /datadrive/jknott/FasterTransformer
+mkdir -p build
+cd build
+git submodule init && git submodule update
 cmake -DSM=70 -DCMAKE_BUILD_TYPE=Release -DBUILD_PYT=ON -DBUILD_MULTI_GPU=ON ..
 make
