212a639b98a48d5d98ac1339b0f30e4a8e4343a2
Julianne Knott <jknott@microsoft.com>
2022-09-24 01:15:51 +0000

    a100 specific

--- rebuild.sh
+++ rebuild.sh
@@ -2,7 +2,7 @@ rm -rf build
 mkdir -p build
 cd build
 git submodule init && git submodule update
-cmake -DSM=70 -DCMAKE_BUILD_TYPE=Release -DBUILD_PYT=ON -DBUILD_MULTI_GPU=ON ..
+cmake -DSM=80 -DCMAKE_BUILD_TYPE=Release -DBUILD_PYT=ON -DBUILD_MULTI_GPU=ON ..
 make
 pip install -r ../examples/pytorch/requirement.txt
 wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-vocab.json -P ../models
